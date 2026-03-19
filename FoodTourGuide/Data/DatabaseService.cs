bdf6e3a33a5b5a13adc4526e10b6f972d4b20293
viettuong287 <voviettuong28072005@gmail.com>
2026-03-19 21:13:26 +0700

    Add project files, ignore IDE/build artifacts

---
+++ FoodTourGuide/Data/DatabaseService.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using FoodTourGuide.Models;
+
+namespace FoodTourGuide.Data
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection _db;
+
+        public async Task Init()
+        {
+            if (_db != null) return;
+
+            var path = Path.Combine(FileSystem.AppDataDirectory, "foodtour.db");
+            _db = new SQLiteAsyncConnection(path);
+
+            await _db.CreateTableAsync<PoI>();
+        }
+
+        public SQLiteAsyncConnection GetDb()
+        {
+            return _db;
+        }
+    }
+}
