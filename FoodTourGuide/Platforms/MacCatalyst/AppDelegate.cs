3eaa4f0598fd71fc33c4ff1b9996ae39b73f1154
viettuong287 <voviettuong28072005@gmail.com>
2026-03-19 20:41:28 +0700

    "Initial commit"

---
+++ FoodTourGuide/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FoodTourGuide
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
